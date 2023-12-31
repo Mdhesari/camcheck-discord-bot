104c09e5147d49b6312a4474ca60bde23a77e632
Mohamad Hesari <mdhesari99@gmail.com>
2023-12-31 23:45:11 +0330

    fix(entity): channel bson

--- entity/channel.go
+++ entity/channel.go
@@ -5,7 +5,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Channel struct {
 	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	DiscordID string             `json:"discord_id,omitempty" bson:"discord_id,omitempty"`
-	GuildID   string             `json:"guild_id,omitempty"`
-	Name      string             `json:"name,omitempty"`
-	IsVideo   bool               `json:"is_video,omitempty"`
+	GuildID   string             `json:"guild_id,omitempty" bson:"guild_id,omitempty"`
+	Name      string             `json:"name,omitempty" bson:"name,omitempty"`
+	IsVideo   bool               `json:"is_video,omitempty" bson:"is_video,omitempty"`
 }
