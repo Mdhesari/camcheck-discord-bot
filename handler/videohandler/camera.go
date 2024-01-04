8351404e01606ea1e59a5dad9062d24bf4807816
Mohamad Hesari <mdhesari99@gmail.com>
2024-01-04 12:14:51 +0330

    camera: goroutine check if user is in the same channel

--- handler/videohandler/camera.go
+++ handler/videohandler/camera.go
@@ -27,23 +27,27 @@ func (h Handler) CheckCameraAndDisconnect(s *discordgo.Session, e *discordgo.Voi
 	if !e.SelfVideo && e.ChannelID != "" {
 		h.channelSrv.AddUserCameraOff(e.ChannelID, e.UserID)
 
-		go func() {
+		go func(channelId string, userId string) {
+			log.Println("self video: ", e.SelfVideo)
+
 			time.Sleep(10 * time.Second)
 
-			if !h.channelSrv.IsUserCameraOn(e.ChannelID, e.UserID) {
+			if !h.channelSrv.IsUserCameraOn(channelId, userId) {
 				msg := fmt.Sprintf("%s Your camera is off! You will be disconnected very soon!\nThat's all I know...", e.Member.User.Mention())
 
-				s.ChannelMessageSend(e.ChannelID, msg)
+				s.ChannelMessageSend(channelId, msg)
 			}
 
 			time.Sleep(time.Duration(h.config.MaxWaitSeconds) * time.Second)
 
-			if !h.channelSrv.IsUserCameraOn(e.ChannelID, e.UserID) {
-				if err := s.GuildMemberMove(e.GuildID, e.UserID, nil); err != nil {
+			log.Println("self video: ", e.SelfVideo)
+
+			if !h.channelSrv.IsUserCameraOn(channelId, userId) && e.ChannelID == channelId {
+				if err := s.GuildMemberMove(channelId, userId, nil); err != nil {
 					log.Println("Failed to disconnect from channel: ", err)
 				}
 			}
-		}()
+		}(e.ChannelID, e.UserID)
 	} else if e.SelfVideo {
 		if !h.channelSrv.IsUserCameraOn(e.ChannelID, e.UserID) {
 			h.channelSrv.RemoveUserCameraOff(e.ChannelID, e.UserID)
